fe79e967382d25998402874fa405a27da39aeeab
odyssic <stevenbuddenjr@gmail.com>
2020-02-07 18:21:20 -0500

    slightly updated still not working

--- javascript notes.js
+++ javascript notes.js
@@ -20,87 +20,87 @@ fetch('http://localhost:3000/notes/')
         noteList.insertAdjacentHTML('afterend', noteHTML)
     });
 
-// function postNewNote(noteText) {
-//     return fetch('http://localhost:3000/notes/', {
-//         method: 'POST',
-//         headers: { 'Content-Type': 'application/json' },
-//         body: JSON.stringify(noteText)
-//     });
+function postNewNote(noteText) {
+    return fetch('http://localhost:3000/notes/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(noteText)
+    });
 
 
 
 
-// }
+    // }
 
-// function renderNewTodo(todo) {
-//     const todoHTML = createTodoHTML(todo)
-//     const todosList = document.querySelector('#todos-list')
-//     todosList.insertAdjacentHTML('beforeend', todoHTML)
-// }
+    // function renderNewTodo(todo) {
+    //     const todoHTML = createTodoHTML(todo)
+    //     const todosList = document.querySelector('#todos-list')
+    //     todosList.insertAdjacentHTML('beforeend', todoHTML)
+    // }
 
-function getAllNotes() {
-    return fetch('http://localhost:3000/todos/', {
-            method: 'GET'
-        })
-        .then(response => response.json())
-}
+    function getAllNotes() {
+        return fetch('http://localhost:3000/todos/', {
+                method: 'GET'
+            })
+            .then(response => response.json())
+    }
 
-let noteHistory = getAllNotes();
+    let noteHistory = getAllNotes();
 
-function noteHistory() {
+    function noteHistory() {
 
-    return response;
-}
-
-
+        return response;
+    }
 
 
 
-// $('#new-todo-form').addEventListener('submit', event => {
-//     event.preventDefault()
-//     const todoTextField = q('#todo-text')
-//     const todoText = todoTextField.value
-//     todoTextField.value = ''
-//     postNewTodo(todoText).then(renderNewTodo)
-// })
-
-// /clintons example
-// function postNewTodo(todoText) {
-//     return fetch('http://localhost:3000/todos/', {
-//             method: 'POST',
-//             headers: { 'Content-Type': 'application/json' },
-//             body: JSON.stringify({ todo: todoText, done: false, created: moment().format() })
-//         })
-//         .then(response => response.json())
-// }
-
-// RIPPLE EVENTS
-//Ripple Event Handler
-// var drawRipple = function(ev) {
-//     var x = ev.clientX;
-//     var y = ev.clientY;
-//     var node = document.querySelector(".ripple");
-//     var newNode = node.cloneNode(true);
-//     newNode.classList.add("animate");
-//     newNode.style.left = ev.clientX - 5 + "px";
-//     newNode.style.top = ev.clientY - 5 + "px";
-//     node.parentNode.replaceChild(newNode, node);
-// };
-
-// //Ripple Triggers
-// window.addEventListener("click", drawRipple);
 
 
-// //Control Handler
-// var controlHandler = function() {
-//     document.body.classList.toggle("dark");
-//     if (document.body.classList.contains("dark")) {
-//         controller.textContent = "Dark Ripple";
-//     } else {
-//         controller.textContent = "Bright Ripple";
-//     }
-// };
+    // $('#new-todo-form').addEventListener('submit', event => {
+    //     event.preventDefault()
+    //     const todoTextField = q('#todo-text')
+    //     const todoText = todoTextField.value
+    //     todoTextField.value = ''
+    //     postNewTodo(todoText).then(renderNewTodo)
+    // })
 
-// // Control Trigger
-// var controller = document.querySelector(".controller");
-// controller.addEventListener("click", controlHandler);
+    // /clintons example
+    // function postNewTodo(todoText) {
+    //     return fetch('http://localhost:3000/todos/', {
+    //             method: 'POST',
+    //             headers: { 'Content-Type': 'application/json' },
+    //             body: JSON.stringify({ todo: todoText, done: false, created: moment().format() })
+    //         })
+    //         .then(response => response.json())
+    // }
+
+    // RIPPLE EVENTS
+    //Ripple Event Handler
+    // var drawRipple = function(ev) {
+    //     var x = ev.clientX;
+    //     var y = ev.clientY;
+    //     var node = document.querySelector(".ripple");
+    //     var newNode = node.cloneNode(true);
+    //     newNode.classList.add("animate");
+    //     newNode.style.left = ev.clientX - 5 + "px";
+    //     newNode.style.top = ev.clientY - 5 + "px";
+    //     node.parentNode.replaceChild(newNode, node);
+    // };
+
+    // //Ripple Triggers
+    // window.addEventListener("click", drawRipple);
+
+
+    // //Control Handler
+    // var controlHandler = function() {
+    //     document.body.classList.toggle("dark");
+    //     if (document.body.classList.contains("dark")) {
+    //         controller.textContent = "Dark Ripple";
+    //     } else {
+    //         controller.textContent = "Bright Ripple";
+    //     }
+    // };
+
+    // // Control Trigger
+    // var controller = document.querySelector(".controller");
+    // controller.addEventListener("click", controlHandler);
--- script.js
+++ script.js
@@ -3,35 +3,71 @@ function print(value) {
     return value
 }
 
+function $(selector) {
+    return document.querySelector(selector)
+}
+
+
+const noteTextField = $("note-text")
+noteText = ''
+const noteText = noteTextField.value
+
+function postNote() {
+    let errorMssg = 'Please enter a note!'
+    if (noteText == '') {
+        return errorMssg
+    } else {
+        postNewNote(noteText)
+        console.log(noteText)
+
+    }
+
+}
+
+
 container.addEventListener("submit", event => {
     event.preventDefault()
+    postNote()
+    console.log('submit button pressed')
 })
 
-// getAllNotes()
-// renderNoteList()
-
-const noteList = document.querySelector('#note-list');
-const notesForList = document.createElement('ul');
-
-fetch('http://localhost:3000/notes/')
-    .then((response) => {
-        return response.json();
-    })
-    .then((myNotes) => {
-        console.log(myNotes);
+fetchAndRenderNotes()
 
 
-        noteList.appendChild(myNotes.title)
+const list = $('#list')
+const newNote = document.createElement('li')
 
+
+function fetchAndRenderNotes() {
+    fetch('http://localhost:3000/notes/')
+        .then((response) => {
+            return response.json();
+
+        })
+        .then((myNotes) => {
+            console.log(myNotes)
+            console.log('notes have been fetched!');
+            return myNotes;
+
+            // let editButton = document.createElement('img')
+            // editButton.src = "vector60-2136-01.jpg"
+
+            list.appendChild(newNote)
+            newNote.innerHTML = myNotes.body
+            console.log(myNotes.body)
+
+        });
+}
+
+function postNewNote(noteText) {
+    return fetch('http://localhost:3000/notes/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            "body": noteText,
+            "created": moment().format()
+
+        })
 
     });
-
-
-
-
-
-// function renderNoteList() {
-//     getAllNotes();
-
-//     const listPlace = document.querySelector('note-list');
-//     // const noteList = document.createElement('ul');
+}
