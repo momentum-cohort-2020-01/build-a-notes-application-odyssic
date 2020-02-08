c0543e57d62f94828ffef7ba49407fab7b1a28ae
odyssic <stevenbuddenjr@gmail.com>
2020-02-08 14:40:43 -0500

    starting over

--- script.js
+++ script.js
@@ -1,73 +1,43 @@
 function print(value) {
-    console.log(value)
-    return value
+    console.log(value);
+    return value;
 }
 
 function $(selector) {
-    return document.querySelector(selector)
+    return document.querySelector(selector);
 }
-
-
-const noteTextField = $("note-text")
-noteText = ''
-const noteText = noteTextField.value
-
-function postNote() {
-    let errorMssg = 'Please enter a note!'
-    if (noteText == '') {
-        return errorMssg
-    } else {
-        postNewNote(noteText)
-        console.log(noteText)
-
-    }
-
-}
-
 
 container.addEventListener("submit", event => {
-    event.preventDefault()
-    postNote()
-    console.log('submit button pressed')
-})
+    event.preventDefault();
+    console.log("submit button pressed");
+});
 
-fetchAndRenderNotes()
-
-
-const list = $('#list')
-const newNote = document.createElement('li')
-
-
-function fetchAndRenderNotes() {
-    fetch('http://localhost:3000/notes/')
-        .then((response) => {
-            return response.json();
-
-        })
-        .then((myNotes) => {
-            console.log(myNotes)
-            console.log('notes have been fetched!');
-            return myNotes;
-
-            // let editButton = document.createElement('img')
-            // editButton.src = "vector60-2136-01.jpg"
-
-            list.appendChild(newNote)
-            newNote.innerHTML = myNotes.body
-            console.log(myNotes.body)
-
-        });
-}
-
-function postNewNote(noteText) {
-    return fetch('http://localhost:3000/notes/', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            "body": noteText,
-            "created": moment().format()
-
-        })
-
+fetch("http://localhost:3000/notes/")
+    .then(resp => resp.json())
+    .then(function(data) {
+        let notes = data;
+        console.log(data);
     });
-}
+
+// fetch('http://localhost:3000/notes/', {
+//   method: 'POST',
+//   headers: {"Content-Type": "application/json"},
+//   body: JSON.stringify({"title": "Hi", "body": "COOL"})
+// })
+// .then(r => r.json())
+// .then(
+//   // whatever you need to do next
+// )
+
+// function postNewNote(noteText) {
+//     return fetch('http://localhost:3000/notes/', {
+//         method: 'POST',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify({
+//             "body": noteText,
+//             "created": moment().format()
+
+//         })
+
+//     });
+// }\
