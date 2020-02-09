fa3c9787335f5c32104e9e4f5bef29d42463799d
odyssic <stevenbuddenjr@gmail.com>
2020-02-09 11:29:30 -0500

    working on form

--- script.js
+++ script.js
@@ -7,9 +7,14 @@ function $(selector) {
     return document.querySelector(selector);
 }
 
-container.addEventListener("submit", event => {
+const form = $("#new-note");
+
+form.addEventListener("submit", event => {
     event.preventDefault();
+    postNewNote();
     console.log("submit button pressed");
+
+    // return false;
 });
 
 getAllNotes();
@@ -29,6 +34,49 @@ function getAllNotes() {
         console.log(data[0].body);
     });
 }
+
+function postNewNote() {
+    fetch("http://localhost:3000/notes/", {
+            method: "POST",
+            body: JSON.stringify({}),
+            headers: { "Content-Type": "application/json" }
+        })
+        .then(function(data) {
+            console.log(data);
+        })
+        .catch(function(data) {
+            console.log(data);
+        });
+}
+
+// const formData = new FormData();
+// const photos = document.querySelector('input[type="file"][multiple]');
+
+// formData.append('title', 'My Vegas Vacation');
+// for (let i = 0; i < photos.files.length; i++) {
+//     formData.append('photos', photos.files[i]);
+// }
+
+// fetch('https://example.com/posts', {
+//             method: 'POST',
+//             body: formData,
+
+// etch("/echo/json/", {
+//         headers: {
+//             Accept: "application/json",
+//             "Content-Type": "application/json"
+//         },
+//         method: "POST",
+//         body: JSON.stringify({ a: 1, b: 2 })
+//     })
+//     .then(function(res) {
+//         console.log(res);
+//     })
+//     .catch(function(res) {
+//         console.log(res);
+//     });
+
+// note data then index of note then .key
 
 // fetch(url)
 //     .then((resp) => resp.json()) // Transform the data into json
