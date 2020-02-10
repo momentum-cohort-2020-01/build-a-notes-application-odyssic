b6d1990f3feea0dc397ddf0690c4c97c4e3298b4
odyssic <stevenbuddenjr@gmail.com>
2020-02-10 14:48:29 -0500

    almost working with non working buttons

--- script.js
+++ script.js
@@ -12,10 +12,12 @@ const submitButton = $("#submit-button");
 
 form.addEventListener("submit", event => {
     event.preventDefault();
-    postNewNote(title, body);
+    const title = $("#title").value;
+    const body = $("#body").value;
     console.log("submit button pressed");
-    console.log("submit button console: title " + $("#title").value);
-    console.log("submit button console: body " + $("#body").value);
+    console.log("submit button console: title " + title);
+    console.log("submit button console: body " + body);
+    postNewNote(title, body);
     $("#new-note").reset();
 
     // return false;
@@ -32,31 +34,56 @@ function getAllNotes() {
         .then(response => response.json())
 
     .then(function(dataSet) {
-        dataSet.map(dataSet => {
-            createList();
+        dataSet.map(note => {
+            createList(note);
         });
     });
 }
+
 const dataSet = {};
 
-function createList() {
+function createList(note) {
     const list = $("#list");
-    const newListItem = document.createElement("li");
-    list.appendChild(newListItem);
-    newListItem.innerText = (dataSet.body, dataSet.title, dataSet.created);
+
+    let listItemContainer = document.createElement("div")
+    list.appendChild(listItemContainer)
+
+    const newTitle = document.createElement("div");
+    const newBody = document.createElement("div");
+    const newDate = document.createElement("div");
+
+    titleDiv = listItemContainer.appendChild(newTitle);
+    bodyDiv = listItemContainer.appendChild(newBody);
+    dateDiv = listItemContainer.appendChild(newDate);
+
+    const deleteButton = document.createElement('button')
+    deleteButton.textContent = 'Delete'
+    listItemContainer.insertAdjacentElement("afterend", deleteButton)
+    deleteButton.classList.add('button-smaller')
+
+    const editButton = document.createElement('button')
+    deleteButton.textContent = 'Edit'
+    listItemContainer.insertAdjacentElement("afterend", editButton)
+    editButton.classList.add('button-smaller')
+
+    titleDiv.classList.add('title')
+    bodyDiv.classList.add('body')
+    dateDiv.classList.add('date')
+
+    newTitle.innerText = `${note.title}`;
+    newBody.innerText = `${note.body}`;
+    newDate.innerText = `${note.created}`;
 }
 
-const title = $("#title").value;
-const body = $("#body").value;
 console.log(moment().format("LLLL"));
 const now = moment().format("LLLL");
 
 function postNewNote(title, body) {
     console.log("postNewNote called");
-    fetch(fetchUrl, {
+    return fetch(fetchUrl, {
 
             method: "POST",
-            headers: { "Content-Type": "application/json" },
+            headers: { "Content-type": "application/json" },
             body: JSON.stringify({
                 title: title,
                 body: body,
